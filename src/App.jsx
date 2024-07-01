b1cf5b7c2f2b00ece5e8ffbdb7bdf0da597740c3
Sudharsan <sudhars4012@gmail.com>
2024-07-01 09:19:09 +0530

    Fixes link to pages and create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/home";
+import Header from "./components/header/header";
+import About from "./pages/about/about";
+import SignUp from "./pages/sign-up/sign-up";
+import SignIn from "./pages/sign-in/sign-in";
+import Profile from "./pages/profile/profile";
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
-        <Route path="" element={<Home />} />
-        <Route path="" element={<Home />} />
-        <Route path="" element={<Home />} />
-        <Route path="" element={<Home />} />
-        <Route path="" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
